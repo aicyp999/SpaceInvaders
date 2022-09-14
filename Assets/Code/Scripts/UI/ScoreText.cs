712c205dbdc3864c99db6be91d611617327b0237
axelc <axel.hpm.coudray@gmail.com>
2022-09-14 11:16:12 +0200

    added live score

--- Assets/Code/Scripts/UI/ScoreText.cs
+++ Assets/Code/Scripts/UI/ScoreText.cs
@@ -5,19 +5,22 @@ using UnityEngine.UI;
 
 public class ScoreText : MonoBehaviour
 {
+    public bool inGame = false;
+
     private int score;
     private Text uiScoreText;
+    private GameObject sceneManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject sceneManager = GameObject.Find("SceneManager");
+        sceneManager = GameObject.Find("SceneManager");
         uiScoreText = GetComponent<Text>();
 
         if (sceneManager != null && uiScoreText != null)
         {
             score = sceneManager.GetComponent<SceneManagement>().getScore();
-            uiScoreText.text = "<YOUR SCORE IS> " + score + " <POINTS>";
+            uiScoreText.text = inGame ? "<enemy destroyed: " + score + "/>" : "<YOUR SCORE IS> " + score + " <POINTS>";
         } else
         {
             uiScoreText.text = "we lost the score... weird...";
@@ -28,6 +31,13 @@ public class ScoreText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (inGame)
+        {
+            if (sceneManager != null && uiScoreText != null)
+            {
+                score = sceneManager.GetComponent<SceneManagement>().getScore();
+                uiScoreText.text = "<enemy destroyed: " + score + "/>";
+            }
+        }
     }
 }
